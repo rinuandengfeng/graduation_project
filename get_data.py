a19d83406c706a2a8a23f5e03eed8828a6431823
rndf <rndf@mail.com>
2024-02-29 19:44:03 +0800

    获取页面数据基本完成

--- get_data.py
+++ get_data.py
@@ -25,56 +25,139 @@ browser.get("https://www.dangdang.com/")
 
 time.sleep(5)
 HEADINFO = (
-    "排名, 书名, 作者, 封面, 出版社时间, 出版社, 翻译者, 推荐率, 现价, 原价,折扣, 大类型, 子类型, 开本, 纸张, 包装, 是否套装, 评论数量, 评论标签, 编辑推荐")
+    "排名, 书名, 作者, 翻译者, 封面, 出版社时间, 出版社, 推荐率, 现价, 定价,折扣, 大类型, 子类型, 开本, 纸张, 包装, 是否套装,ISBN , 评论数量, 评论标签, 编辑推荐")
 
 
 # 获取2023 年所有月份的图书信息
-def get_book_info(year, month):
+def get_book_info(url, csv_name=None, year=None, list_num=1):
     """
-    获取每年各个月份的图书数据
-    :param year: string 年份
-    :param month: string 年份
+    获取图书信息并解析，写入到文件中
+    :param url: string 地址
     :return:
     """
     # 遍历前12个月
     book_info_list = []
-    filename = "./data/" + year + "/book_info" + month + "month.csv"
-    row_url = "http://bang.dangdang.com/books/bestsellers/01.00.00.00.00.00-month-2023-" + \
-              month + "-1-"
+    # csv_name.replace("src", "")
+    # filename = "./data/" + year + "/book_info" + month + "month.csv"
+    #
+    # # 写入csv文件的头
+    # with open(filename, "a", encoding="utf-8", newline="") as f:
+    #     f.write(HEADINFO + "\n")
 
-    # 写入csv文件的头
-    with open(filename, "a", encoding="utf-8", newline="") as f:
-        f.write(HEADINFO + "\n")
+    # 读取csv文件的url
 
-    for page in range(1, 2):
-        url = row_url + str(page)
-        browser.get(url)
+    browser.get(url)
 
-        # 解析要获取的数据
-        # list_num = browser.find_elements(By.CSS_SELECTOR, '<div class="list_num red">1.</div>') # 编号
-        # 排行榜
-        li = browser.find_elements(By.CSS_SELECTOR, 'body > div.bang_list_box > ul > li')
-        print(li)
-        list_num = browser.find_elements(By.CSS_SELECTOR, "div.list_num")  # 编号
-        book_info_list.append(list_num)
-        print(list_num)
+    # 解析要获取的数据
+    # list_num = browser.find_elements(By.CSS_SELECTOR, '<div class="list_num red">1.</div>') # 编号
+    # 排行榜
+    book_info_list.append(list_num)
+    # 书名
+    book_name = browser.find_element(By.XPATH, '//*[@id="product_info"]/div[1]/h1')
+    print("书名：", book_name.get_attribute("title"))
+    book_info_list.append(book_name.get_attribute("title"))
+    # 作者
+    author = browser.find_element(By.XPATH, '//*[@id="author"]')
+    author = author.text.replace("作者:", "")
+    print("作者信息：", author)
+    book_info_list.append(author)
+    # 翻译者
 
-        # 书名
-        pic_row = browser.find_elements(By.CSS_SELECTOR, "div.pic > a")
-        book_info_list.append(pic_row.get_attribute("alt"))
-        # 作者
-        author = browser.find_elements(By.CSS_SELECTOR, "")
+    # 封面图
+    pic = browser.find_element(By.XPATH, '//*[@id="largePic"]')
+    print("封面图：", pic.get_attribute("src"))
+    book_info_list.append(pic.get_attribute("src"))
+    # 出版社时间
+    publication_time = browser.find_element(By.XPATH, '//*[@id="product_info"]/div[2]/span[3]')
+    publication_time = publication_time.text.replace("出版时间:", "")
+    print("出版时间：", publication_time)
+    book_info_list.append(publication_time)
 
-        # 封面
-        book_info_list.append(pic_row.get_attribute("src"))  # 封面图
+    # 出版社
+    press = browser.find_element(By.XPATH, '//*[@id="product_info"]/div[2]/span[2]/a')
+    book_info_list.append(press.text)
 
+    # 推荐率
+
+    # 现价
+    current_price = browser.find_element(By.XPATH, '//*[@id="dd-price"]')
+    current_price = current_price.text.replace("¥", "")
+    print("现价：", current_price)
+    book_info_list.append(current_price)
+    # 定价
+    pricing = browser.find_element(By.XPATH, '//*[@id="original-price"]')
+    pricing = pricing.text.replace("¥", "")
+    print("定价", pricing)
+    book_info_list.append(pricing)
+    # 折扣
+    discount = browser.find_element(By.XPATH, '//*[@id="dd-zhe"]')
+    discount = discount.text.replace("折)", "")
+    discount = discount.replace("(", "")
+
+    print("折扣:", discount)
+    book_info_list.append(discount)
+    # 大类型
+    type_list = browser.find_elements(By.XPATH, '//*[@id="detail-category-path"]/span/a[2]')
+    for i in type_list:
+        print("大类型：", i.text)
+        book_info_list.append(i.text)
+    # 子类型
+    subtype = browser.find_element(By.XPATH, '//*[@id="detail-category-path"]/span/a[3]')
+    print("子类型：", subtype.text)
+    book_info_list.append(subtype.text)
+    # 开本
+    book_size = browser.find_element(By.XPATH, '//*[@id="detail_describe"]/ul/li[1]')
+    book_size = book_size.text.replace("开 本：", "")
+    print("开本：", book_size)
+    book_info_list.append(book_size)
+    # 纸张
+    paper = browser.find_element(By.XPATH, '//*[@id="detail_describe"]/ul/li[2]')
+    paper = paper.text.replace("纸 张：", "")
+    print("纸张：", paper)
+    book_info_list.append(paper)
+    # 包装
+    packaging = browser.find_element(By.XPATH, '//*[@id="detail_describe"]/ul/li[3]')
+    packaging = packaging.text.replace("包 装：", "")
+    print("包装：", packaging)
+    book_info_list.append(packaging)
+    # 是否套装
+    suit = browser.find_element(By.XPATH, '//*[@id="detail_describe"]/ul/li[4]')
+    suit = suit.text.replace("是否套装：", "")
+    print("是否套装：", suit)
+    book_info_list.append(suit)
+    # 国际标准书号ISBN
+    isbn = browser.find_element(By.XPATH, '//*[@id="detail_describe"]/ul/li[5]')
+    isbn = isbn.text.replace("国际标准书号ISBN：", "")
+    print("国际标准书号ISBN：", isbn)
+    book_info_list.append(isbn)
+    # 评论数量
+    comment_num = browser.find_element(By.XPATH, '//*[@id="comm_num_down"]')
+    print(comment_num.text)
+    print("评论数量：", comment_num.text)
+    book_info_list.append(comment_num.text)
+    # 评论标签
+    comment_tag = browser.find_elements(By.XPATH, '//*[@id="comment_tags_div"]/div[2]/span')
+    print("评论标签：", comment_tag)
+    for i in comment_tag:
+        comment_tag_list = []
+        info = i.find_element(By.XPATH, 'a').text
+        comment_tag_list.append(info)
+        print("评论标签：", comment_tag_list)
+        book_info_list.append(comment_tag_list)
+
+    # 编辑推荐
+    editor_recommend = browser.find_element(By.XPATH, '//*[@id="abstract"]/div[2]')
+    print("编辑推荐：", editor_recommend.text)
+    book_info_list.append(editor_recommend.text)
+
+    print(book_info_list)
     # 写入到csv文件中
-    logger.info("开始获取2023年" + month + "月的图书信息到文件中...")
-    with open(filename, "a", encoding="utf-8", newline="") as f:
-        print(book_info_list)
-        writer = csv.writer(f)
-        writer.writerows(book_info_list)
-    logger.info("写入2023年" + month + "月的图书信息到文件中结束")
+    # logger.info("开始获取2023年" + month + "月的图书信息到文件中...")
+    # with open(filename, "a", encoding="utf-8", newline="") as f:
+    #     print(book_info_list)
+    #     writer = csv.writer(f)
+    #     writer.writerows(book_info_list)
+    # logger.info("写入2023年" + month + "月的图书信息到文件中结束")
 
     return
 
@@ -82,8 +165,8 @@ def get_book_info(year, month):
 def get_num_src(year, month):
     # 遍历前12个月
 
-    filename = "./data/" + year + "/book_info_" + month + "_month_src.csv"
-    row_url = "http://bang.dangdang.com/books/bestsellers/01.00.00.00.00.00-month-2023-" + \
+    filename = "./data/" + year + "/bests_sellers_" + month + "_month_src.csv"
+    row_url = "http://bang.dangdang.com/books/bestsellers/01.00.00.00.00.00-year-" + year + "-" + \
               month + "-1-"
     num_src = ('排名,图书地址')
     # 写入csv文件的头
@@ -119,6 +202,7 @@ def get_num_src(year, month):
 
 if __name__ == "__main__":
     time.sleep(3)
-    # get_book_info("2023", "2")
+    get_book_info("http://product.dangdang.com/29311943.html")
 
-    get_num_src("2023", "6")
+    # for i in range(2020, 2024):
+    #     get_num_src(str(i), "0")
